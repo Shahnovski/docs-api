02cc46cf8d3a218202f86dd5e64012e69372ffa2
Shahnovski <shahnovski300899@gmail.com>
2021-01-11 14:47:21 +0300

    Initial commit

---
+++ src/main/java/com/example/docksapi/DocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.docksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocksApiApplication.class, args);
+	}
+
+}
