a5284e5a8d16f626fd8957ec23a95c1db478cde8
Shahnovski <shahnovski300899@gmail.com>
2021-01-11 19:30:04 +0300

    Renamed files

--- src/main/java/com/example/docksapi/DocsApiApplication.java
+++ src/main/java/com/example/docksapi/DocsApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocksApiApplication {
+public class DocsApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocksApiApplication.class, args);
+		SpringApplication.run(DocsApiApplication.class, args);
 	}
 
 }
