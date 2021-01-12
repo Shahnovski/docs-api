f14391ec741c65d478066ba591292b8e3f424b63
Shahnovski <shahnovski300899@gmail.com>
2021-01-12 12:59:00 +0300

    Added annotations for validation

--- src/main/java/com/example/docksapi/doc/Doc.java
+++ src/main/java/com/example/docksapi/doc/Doc.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.io.File;
 import java.util.Date;
 
@@ -21,12 +22,16 @@ public class Doc {
     private Long id;
 
     @Column(name = "docName", nullable = false)
+    @NotBlank(message = "DocName should not be empty")
+    @Size(min = 5, max = 20, message = "DocName should be between 5 and 20 characters")
     private String docName;
 
     @Column(name = "docCreationDate", nullable = false)
+    @NotNull(message = "DocCreationDate should not be empty")
     private Date docCreationDate;
 
     @Column(name = "docDeadlineDate", nullable = false)
+    @NotNull(message = "DocDeadlineDate should not be empty")
     private Date docDeadlineDate;
 
     @Column(name = "docStatus", nullable = false)
@@ -37,13 +42,24 @@ public class Doc {
     private String docAuthorName;
 
     @Column(name = "docClientName", nullable = false)
+    @NotBlank(message = "DocClientName should not be empty")
+    @Size(min = 5, max = 50, message = "DocClientName should be between 5 and 50 characters")
     private String docClientName;
 
     @Column(name = "docWorkerName", nullable = false)
+    @NotBlank(message = "DocWorkerName should not be empty")
+    @Size(min = 5, max = 50, message = "DocWorkerName should be between 5 and 50 characters")
     private String docWorkerName;
 
+    @Column(name = "docFileName", nullable = false)
+    @NotBlank(message = "DocFileName should not be empty")
+    @Size(min = 5, max = 20, message = "DocFileName should be between 5 and 20 characters")
+    private String docFileName;
+
     @Lob
-    @Column(name = "docFile", nullable = false)
-    private byte[] docFile;
+    @Column(name = "docFileContent", nullable = false)
+    @NotNull(message = "DocFileContent should not be empty")
+    @Size(min = 1, message = "DocFileContent should not be empty")
+    private byte[] docFileContent;
 
 }
--- src/main/java/com/example/docksapi/doc/DocDTO.java
+++ src/main/java/com/example/docksapi/doc/DocDTO.java
@@ -4,19 +4,44 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocDTO {
+
     private Long id;
+
+    @NotBlank(message = "DocName should not be empty")
+    @Size(min = 5, max = 20, message = "DocName should be between 5 and 20 characters")
     private String docName;
+
+    @NotNull(message = "DocCreationDate should not be empty")
     private Date docCreationDate;
+
+    @NotNull(message = "DocDeadlineDate should not be empty")
     private Date docDeadlineDate;
+
     private DocStatus docStatus;
+
     private String docAuthorName;
+
+    @NotBlank(message = "DocClientName should not be empty")
+    @Size(min = 5, max = 50, message = "DocClientName should be between 5 and 50 characters")
     private String docClientName;
+
+    @NotBlank(message = "DocWorkerName should not be empty")
+    @Size(min = 5, max = 50, message = "DocWorkerName should be between 5 and 50 characters")
     private String docWorkerName;
-    private byte[] docFile;
+
+    @NotBlank(message = "DocFileName should not be empty")
+    @Size(min = 5, max = 20, message = "DocFileName should be between 5 and 20 characters")
+    private String docFileName;
+
+    @NotNull(message = "DocFileContent should not be empty")
+    @Size(min = 1, message = "DocFileContent should not be empty")
+    private byte[] docFileContent;
 }
